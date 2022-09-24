43879eebdafd44efd38d59019d8c64a7543522bf
AbrarAlzubaidi <abraralzobaide98@gmail.com>
2022-09-24 14:57:47 +0300

    rename prjects

--- practice5/src/Box.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-export default function Box(props){
-    const styles={
-        backgroundColor: props.on ? "#222222" : "none"
-    }
-    return(
-        <div className="box" style={styles}></div>
-    )
-}
